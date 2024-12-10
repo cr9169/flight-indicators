91efa7978ef0cc9e77a3b5d1ab50cc35e8ebf325
cr9169 <bar.gabay.home@gmail.com>
2024-12-10 21:28:19 +0200

    feat: created interface and mongoose model to Indicators

---
+++ server/src/model.ts
@@ -0,0 +1,13 @@
+import mongoose, { Schema } from "mongoose";
+import { Indicators } from "./interface";
+
+const IndicatorsSchema: Schema = new Schema({
+  altitude: { type: Number, required: true },
+  his: { type: Number, required: true },
+  adi: { type: Number, required: true },
+});
+
+export const IndicatorsModel = mongoose.model<Indicators>(
+  "Indicator",
+  IndicatorsSchema
+);
