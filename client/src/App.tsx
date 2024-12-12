4c5983f50b41a07163b711ba7c4e1b5a3957251d
cr9169 <bar.gabay.home@gmail.com>
2024-12-12 22:44:35 +0200

    feat: added base App build, states, and functions, to handle DOM actions, added View type, and NavbarProps to Navbar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,40 @@
 import "./App.css";
+import { useRef, useState } from "react";
+import Navbar from "./components/Navbar/Navbar";
+import { View } from "./types";
+import VisualViewSection from "./components/VisualViewSection/VisualViewSection";
+import TextViewSection from "./components/TextViewSection/TextViewSection";
+import InputDialog from "./components/InputDialog/InputDialog";
 
 function App() {
-  return <></>;
+  const [preferedView, setPreferedView] = useState<View>("text");
+  const [isDialogOpen, setIsDialogOpen] = useState<boolean>(false);
+
+  const dialogRef = useRef<HTMLDialogElement>(null);
+
+  const handleViewChange = (viewType: View) => {
+    setPreferedView(viewType === "text" ? "text" : "visual");
+  };
+
+  const openDialog = () => {
+    setIsDialogOpen(true);
+    dialogRef.current?.showModal();
+  };
+
+  const closeDialog = () => {
+    setIsDialogOpen(false);
+    dialogRef.current?.close();
+  };
+
+  return (
+    <div>
+      {isDialogOpen ? <InputDialog closeDialog={closeDialog} /> : null}
+      <Navbar handleViewChange={handleViewChange} openDialog={openDialog} />
+      <main>
+        {preferedView === "text" ? <VisualViewSection /> : <TextViewSection />}
+      </main>
+    </div>
+  );
 }
 
 export default App;
--- client/src/components/Navbar/Navbar.tsx
+++ client/src/components/Navbar/Navbar.tsx
@@ -1,6 +1,12 @@
+import { View } from "../../types";
 import "./Navbar.scss";
 
-const Navbar = ({}: {}) => {
+interface NavbarProps {
+  handleViewChange: (viewType: View) => void;
+  openDialog: () => void;
+}
+
+const Navbar: React.FC<NavbarProps> = ({ handleViewChange, openDialog }) => {
   return <div></div>;
 };
 
