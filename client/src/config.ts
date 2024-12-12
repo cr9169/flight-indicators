68e9180d9002371a67efb467c72541a504de6945
cr9169 <bar.gabay.home@gmail.com>
2024-12-12 16:21:05 +0200

    feat: added client config

---
+++ client/src/config.ts
@@ -0,0 +1,11 @@
+const config = {
+  service: {
+    port: 4000,
+    host: "0.0.0.0",
+  },
+  server: {
+    clientConnectionString: "http://localhost:5000/api/indicators",
+  },
+};
+
+export default config;
