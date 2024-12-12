47e9ab278a1033dfc7fb1ff881b16ae745bb87f2
cr9169 <bar.gabay.home@gmail.com>
2024-12-12 23:39:33 +0200

    featL added base DOM structure to InputDialog, and added classes to scss file

--- client/src/components/InputDialog/InputDialog.tsx
+++ client/src/components/InputDialog/InputDialog.tsx
@@ -1,11 +1,28 @@
 import "./InputDialog.scss";
+import FieldTitle from "./FieldTitle/FieldTitle";
+import InputBox from "./InputBox/InputBox";
+import SubmitButton from "./SubmitButton/SubmitButton";
 
 interface InputDialogProps {
   closeDialog: () => void;
 }
 
 const InputDialog: React.FC<InputDialogProps> = ({ closeDialog }) => {
-  return <div></div>;
+  return (
+    <dialog className="input-dialog-main">
+      <div className="inputs-section">
+        {["Altitude", "HIS", "ADI"].map((indicator) => {
+          return (
+            <div className="input-section">
+              <FieldTitle />
+              <InputBox />
+            </div>
+          );
+        })}
+      </div>
+      <SubmitButton />
+    </dialog>
+  );
 };
 
 export default InputDialog;
