f2330c3ea9ed0a259f0bd14a4ac6266f224a7f60
cr9169 <bar.gabay.home@gmail.com>
2024-12-12 11:44:51 +0200

    feat: removed version key from mongoose model

--- server/src/Indicators/model.ts
+++ server/src/Indicators/model.ts
@@ -1,11 +1,14 @@
 import mongoose, { Schema } from "mongoose";
 import { Indicators } from "./interface";
 
-const IndicatorsSchema: Schema = new Schema({
-  altitude: { type: Number, required: true },
-  his: { type: Number, required: true },
-  adi: { type: Number, required: true },
-});
+const IndicatorsSchema: Schema = new Schema(
+  {
+    altitude: { type: Number, required: true },
+    his: { type: Number, required: true },
+    adi: { type: Number, required: true },
+  },
+  { versionKey: false }
+);
 
 export const IndicatorsModel = mongoose.model<Indicators>(
   "Indicator",
