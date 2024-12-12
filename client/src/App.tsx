22e4ed5866e2777304e76f4a48666ee8b9df92c1
cr9169 <bar.gabay.home@gmail.com>
2024-12-12 23:02:35 +0200

    feat: added data fetching, and frontend error and waiting for fetching handling

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,28 @@
 import "./App.css";
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import Navbar from "./components/Navbar/Navbar";
 import { View } from "./types";
 import VisualViewSection from "./components/VisualViewSection/VisualViewSection";
 import TextViewSection from "./components/TextViewSection/TextViewSection";
 import InputDialog from "./components/InputDialog/InputDialog";
+import { Indicators } from "./interfaces/indicators";
+import IndicatorsService from "./indicators.service";
 
 function App() {
+  const [indicators, setIndicators] = useState<Indicators | null>(null);
+  const [error, setError] = useState<string | null>(null);
   const [preferedView, setPreferedView] = useState<View>("text");
   const [isDialogOpen, setIsDialogOpen] = useState<boolean>(false);
+
+  useEffect(() => {
+    const fetchData = () => {
+      IndicatorsService.getIndicators()
+        .then((data) => setIndicators(data))
+        .catch((err) => setError(err));
+    };
+
+    fetchData();
+  }, []);
 
   const dialogRef = useRef<HTMLDialogElement>(null);
 
@@ -26,14 +40,22 @@ function App() {
     dialogRef.current?.close();
   };
 
-  return (
+  return error ? (
+    <p>Error: {error}</p>
+  ) : indicators ? (
     <div>
       {isDialogOpen ? <InputDialog closeDialog={closeDialog} /> : null}
       <Navbar handleViewChange={handleViewChange} openDialog={openDialog} />
       <main>
-        {preferedView === "text" ? <VisualViewSection /> : <TextViewSection />}
+        {preferedView === "text" ? (
+          <VisualViewSection indicators={indicators} />
+        ) : (
+          <TextViewSection indicators={indicators} />
+        )}
       </main>
     </div>
+  ) : (
+    <p>Loading...</p>
   );
 }
 
