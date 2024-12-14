64bb6433e19db959865638b463a21f30f509d8c4
cr9169 <bar.gabay.home@gmail.com>
2024-12-14 19:11:59 +0200

    feat: added range validation for indicators values in server

--- server/src/Indicators/validation.ts
+++ server/src/Indicators/validation.ts
@@ -1,9 +1,9 @@
 import { z } from "zod";
 
 export const indicatorsSchema = z.object({
-  altitude: z.number(),
-  his: z.number(),
-  adi: z.number(),
+  altitude: z.number().min(0).max(3000),
+  his: z.number().min(-100).max(100),
+  adi: z.number().min(0).max(100),
 });
 
 export const getIndicatorsSchema = z.object({
