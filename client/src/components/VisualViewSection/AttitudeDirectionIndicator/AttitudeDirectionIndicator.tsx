dce35be045438c5e8fe96a23862b699ab73b2a5c
cr9169 <bar.gabay.home@gmail.com>
2024-12-14 20:40:23 +0200

    feat: added green to app.css to handle adi = 0, and finished HorizontalSituationIndicator and AttitudeDirectionIndicator

--- client/src/components/VisualViewSection/AttitudeDirectionIndicator/AttitudeDirectionIndicator.tsx
+++ client/src/components/VisualViewSection/AttitudeDirectionIndicator/AttitudeDirectionIndicator.tsx
@@ -4,10 +4,16 @@ interface AttitudeDirectionIndicatorProps {
   indicator: number;
 }
 
-const AttitudeDirectionIndicator: React.FC<AttitudeDirectionIndicatorProps> = (
-  indicator
-) => {
-  return <div className="altitude-direction-indicator-main"></div>;
+const AttitudeDirectionIndicator: React.FC<AttitudeDirectionIndicatorProps> = ({
+  indicator,
+}) => {
+  return (
+    <div
+      className={`altitude-direction-indicator-main ${
+        indicator === 0 ? "zero" : indicator === 100 ? "hundred" : ""
+      }`}
+    ></div>
+  );
 };
 
 export default AttitudeDirectionIndicator;
--- client/src/components/VisualViewSection/HorizontalSituationIndicator/HorizontalSituationIndicator.tsx
+++ client/src/components/VisualViewSection/HorizontalSituationIndicator/HorizontalSituationIndicator.tsx
@@ -1,4 +1,6 @@
 import "./HorizontalSituationIndicator.scss";
+import NorthIcon from "@mui/icons-material/North";
+import { useEffect, useState } from "react";
 
 interface HorizontalSituationIndicatorProps {
   indicator: number;
@@ -6,8 +8,35 @@ interface HorizontalSituationIndicatorProps {
 
 const HorizontalSituationIndicator: React.FC<
   HorizontalSituationIndicatorProps
-> = (indicator) => {
-  return <div className="horizontal-situation-indicator-main"></div>;
+> = ({ indicator }) => {
+  const [rotation, setRotation] = useState(0);
+
+  useEffect(() => {
+    const difference = indicator - rotation;
+    const shortestRotation =
+      Math.abs(difference) > 180
+        ? difference > 0
+          ? difference - 360
+          : difference + 360
+        : difference;
+
+    setRotation((prev) => prev + shortestRotation);
+  }, [indicator]);
+
+  return (
+    <div className="horizontal-situation-indicator-main">
+      <p>0</p>
+      <p>180</p>
+      <p>90</p>
+      <p>270</p>
+      <NorthIcon
+        className="arrow"
+        style={{
+          transform: `rotate(${rotation}deg)`,
+        }}
+      />
+    </div>
+  );
 };
 
 export default HorizontalSituationIndicator;
