13aa8d020801f2a4089916c10cfbcd1fb089f366
cr9169 <bar.gabay.home@gmail.com>
2024-12-15 11:15:24 +0200

    fix: fixed HorizontalSituationIndicator rotation functionality

--- client/src/components/VisualViewSection/HorizontalSituationIndicator/HorizontalSituationIndicator.tsx
+++ client/src/components/VisualViewSection/HorizontalSituationIndicator/HorizontalSituationIndicator.tsx
@@ -12,22 +12,24 @@ const HorizontalSituationIndicator: React.FC<
   const [rotation, setRotation] = useState(0);
 
   useEffect(() => {
-    const difference = indicator - rotation;
-    const shortestRotation =
-      Math.abs(difference) > 180
-        ? difference > 0
-          ? difference - 360
-          : difference + 360
-        : difference;
+    setRotation((prev) => {
+      const difference = indicator - prev;
+      const shortestRotation =
+        Math.abs(difference) > 180
+          ? difference > 0
+            ? difference - 360
+            : difference + 360
+          : difference;
 
-    setRotation((prev) => prev + shortestRotation);
+      return prev + shortestRotation;
+    });
   }, [indicator]);
 
   return (
     <div className="horizontal-situation-indicator-main">
       <p>0</p>
-      <p>180</p>
       <p>90</p>
+      <p>180</p>
       <p>270</p>
       <NorthIcon
         className="arrow"
